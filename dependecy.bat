d15e24a73f4920bc86fe8e6b1fbf57907f432b53
An <53161441+anbinhpham1107@users.noreply.github.com>
2020-10-14 14:53:33 -0500

    Modifying the batch file

--- dependecy.bat
+++ dependecy.bat
@@ -1,5 +1,6 @@
 python -m pip install SpeechRecognition
-python -m pip install pyaudio
+python -m pip install pipwin 
+python -m pipwin install pyaudio
 python -m pip install pocketsphinx
 echo Done!
 pause
