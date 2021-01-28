d1629af91ea9871b1fcdd4d870cb58e743423f00
Steven Tran <StevenDoTran@gmail.com>
2021-01-27 21:04:18 -0600

    Added VoiceTraining functionality and adjusted a few lines of code in voice_recognition.py to work with it

---
+++ VoiceTraining/recordAudio.py
@@ -0,0 +1,67 @@
+# Program to read a .transcription file from a specified directory and record the user speaking each line from them.
+
+import os
+import wave
+
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+path = ""
+pathExists = False
+while not pathExists:
+    dirName = input("Input complete name of directory containing the fileids and transcription file.\n")
+    isdir = os.path.isdir(path + dirName)
+    if isdir == True:
+        pathExists = True
+        dirPath = path + dirName
+    else:
+        print("Directory not found. Please try again.")
+        
+fileExists = False
+while not fileExists:
+    fileName = input("Input name of fileids and transcription files without extension.\n")
+    transName = fileName + ".transcription"
+    transPath = dirPath + "/" + transName
+    isfile = os.path.isfile(transPath)
+    if isfile == False:
+        print("Both files not found.\n")
+        continue
+    fileExists = True
+
+transFile = open(transPath, "r")
+fileLines = transFile.read().split("\n")
+fileLines.pop()
+transFile.close()
+
+duration = 8
+freq = 16000
+print("You have " + str(duration) + " seconds to say each line up to the \":\" symbol.")
+
+for fileLine in fileLines:
+    line = fileLine
+    index = line.index("<",4)
+    lastIndex = line.index(")", index + 6)
+    fileName = line[index + 6 : lastIndex]
+
+    goodRecording = False
+    while not goodRecording:
+        print()
+        print(line[4:index] + ": " + fileName)
+
+    
+        recording = sd.rec(int(duration * freq), samplerate = freq, channels = 1, dtype="int16")
+        sd.wait()
+        write(fileName + ".wav", freq, recording)
+
+        os.system("play " + fileName + ".wav")
+        userInput = input("Is the recording fine? (Y/N): ")
+        if userInput == "Y" or userInput == "y":
+            goodRecording = True
+            os.system("mv " + fileName + ".wav " + dirPath)
+        else:
+            print("Retaking recording, please resay the line.")
+            os.system("rm " + fileName + ".wav")
+
+
+print("Recording finished.")
+
