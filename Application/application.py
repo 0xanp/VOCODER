122e124570172bf39b1c8a382d29408b1abf7b1f
An <53161441+anbinhpham1107@users.noreply.github.com>
2021-02-11 15:40:34 -0600

    Making the header image scalable and adding a file system

--- Application/application.py
+++ Application/application.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter.filedialog import *
 from PIL import Image, ImageTk
 import voice_recognition as vr
 import multiprocessing as mp
@@ -7,37 +8,114 @@ import threading
 import queue
 import os
 import sys
+from screeninfo import get_monitors
 
-class Application(tk.Frame):
+class Application:
     """This is the documentation for Application"""
-    def __init__(self, master=None):
-        """ inits Application with a complete UI
-        """
-        super().__init__(master)
-        self.master = master
-        self.grid(sticky=tk.N+tk.S+tk.E+tk.W)
+    root = tk.Tk()
+    width = 1600
+    height = 900
+    menu_bar = tk.Menu(root)
+    menu_file = tk.Menu(menu_bar, tearoff=0)
+    menu_edit = tk.Menu(menu_bar, tearoff=0)
+    menu_voice = tk.Menu(menu_bar, tearoff=0)
+    menu_help = tk.Menu(menu_bar, tearoff=0)
+    master = tk.Frame(root)
+    txt_editor_field = tk.Text(master)
+    file = None    
+    
+    def __init__(self,**kwargs):
+        # Set icon 
+        try: 
+            self.root.wm_iconbitmap("Notepad.ico")  
+        except: 
+            pass
+  
+        # Set window size (the default is 300x300) 
+  
+        try: 
+            self.thisWidth = kwargs['width'] 
+        except KeyError: 
+            pass
+  
+        try: 
+            self.thisHeight = kwargs['height'] 
+        except KeyError: 
+            pass
+
+        # Set the window text 
+        self.root.title("Untitled - VOCODER") 
+
+        # Center the window 
+        screenWidth = self.root.winfo_screenwidth() 
+        screenHeight = self.root.winfo_screenheight() 
+      
+        # For left-alling 
+        left = (screenWidth / 2) - (self.width / 2)  
+          
+        # For right-allign 
+        top = (screenHeight / 2) - (self.height /2)  
+          
+        # For top and bottom 
+        '''
+        self.root.geometry('%dx%d+%d+%d' % (self.width, 
+                                              self.height, 
+                                              left, top))  
+        '''
+        self.root.geometry(f"{self.thisWidth}x{self.thisHeight}")
+        self.root.grid_rowconfigure(0, weight=1) 
+        self.root.grid_columnconfigure(0, weight=1) 
+
+        self.master.grid(sticky=tk.N+tk.S+tk.E+tk.W)
+
         # Making the App window resizeable
-        top=self.winfo_toplevel()                
+        top=self.master.winfo_toplevel()                
         top.rowconfigure(0, weight=1)            
-        top.columnconfigure(0, weight=1)         
-        self.rowconfigure(0, weight=1)           
-        self.columnconfigure(0, weight=1)
+        top.columnconfigure(0, weight=1)  
+        self.master.rowconfigure(0, weight=1)           
+        self.master.columnconfigure(0, weight=1)
 
         ## Header
-        self.header = tk.Frame(self, borderwidth=2, relief="ridge")
-        load = Image.open("assets/vocoder_icon_title.png")
-        render = ImageTk.PhotoImage(load)
-        img = tk.Label(self.header,image=render)
-        img.image = render
-        img.pack()
+        self.header = tk.Frame(self.master, borderwidth=2, relief="ridge")
+        self.render = ImageTk.PhotoImage(file="assets/vocoder_icon_title.png")
+        self.img = tk.Label(self.header,image=self.render)
+        self.img.pack()
         self.header.grid(row=0,padx=5,pady=5,sticky='nsew')
+        self.header.bind("<Configure>", self.image_resizer)
+        ## Menu Bar
+        
+        ## Add functionalities to Menu options
+
+        # File option
+        self.menu_file.add_command(label="New", command=self.newFile)
+        self.menu_file.add_command(label="Open", command=self.openFile)
+        self.menu_file.add_command(label="Save", command=self.saveFile)
+        self.menu_file.add_separator()
+        self.menu_file.add_command(label="Exit", command=self.on_closing) 
+        self.menu_bar.add_cascade(label="File", menu=self.menu_file)
+        
+        # Edit option
+        self.menu_edit.add_command(label="Cut", command=self.cut)
+        self.menu_edit.add_command(label="Copy", command=self.copy)
+        self.menu_edit.add_command(label="Paste", command=self.paste)
+        self.menu_bar.add_cascade(label="Edit", menu=self.menu_edit)
+        
+        # Voice option
+        self.menu_voice.add_command(label="Set up voice", command=self.setUpVoice)
+        self.menu_bar.add_cascade(label="Voice", menu = self.menu_voice)
+
+        # Help option
+        self.menu_help.add_command(label="About Notepad", command=self.showAbout)
+        self.menu_bar.add_cascade(label="Help", menu=self.menu_help)
+        
+        self.root.config(menu=self.menu_bar)
         
         ## App_Layout
-        self.app_layout = tk.Frame(self,width = 1727, height=700, borderwidth=2, relief="ridge")
+        self.app_layout = tk.Frame(self.master,width = self.thisWidth, height=int(self.thisHeight*0.8), borderwidth=2, relief="ridge")
         self.app_layout.grid_propagate(False)
         
         ## Voice recorder Frame
-        self.voice_recog = tk.LabelFrame(self.app_layout, text = "Voice Recorder",width=600, height=200, borderwidth=2, relief="ridge")
+        self.voice_recog = tk.LabelFrame(self.app_layout, text = "Voice Recorder",width=int(self.app_layout.winfo_width()*0.3), height=200, borderwidth=2, relief="ridge")
         self.voice_recog.grid_propagate(False)
         self.indicator = tk.Frame(self.voice_recog, width = 2, height=2, borderwidth=0, relief="ridge")
         self.loadgray = Image.open("assets/grayCircle.jpg")
@@ -64,7 +142,7 @@ class Application(tk.Frame):
         self.cmd_man_txt = tk.Listbox(self.cmd_man)
 
         ## Text Editor
-        self.txt_editor = tk.LabelFrame(self.app_layout,text="Text Editor",width=1112, height=400,borderwidth=2, relief="ridge")
+        self.txt_editor = tk.LabelFrame(self.app_layout,text="Text Editor",width=int(self.app_layout.winfo_width()*0.7), height=400,borderwidth=2, relief="ridge")
         self.txt_editor.grid_propagate(False)
         self.txt_editor_field = tk.Text(self.txt_editor)
 
@@ -118,10 +196,85 @@ class Application(tk.Frame):
 
         ## Packing all the widgets in Help
         self.help_field.grid(row=3,columnspan=3,padx=5,pady=5,sticky='nsew')
+        
+        
+    def showAbout(self):
+        tk.messagebox.showinfo("VOCODER", "Binh An Pham\nM Rachel Vanpelt\nSteven Tran")
     
+    def openFile(self):
+        self.file = askopenfilename(defaultextension=".txt", 
+                                      filetypes=[("All Files","*.*"), 
+                                        ("Text Documents","*.txt")]) 
+  
+        if self.file == "": 
+              
+            # no file to open 
+            self.file = None
+        else: 
+              
+            # Try to open the file 
+            # set the window title 
+            self.root.title(os.path.basename(self.file) + " - VOCODER") 
+            self.txt_editor_field.delete(1.0,tk.END) 
+  
+            file = open(self.file,"r") 
+  
+            self.txt_editor_field.insert(1.0,file.read()) 
+  
+            file.close() 
+
+    def newFile(self): 
+        self.root.title("Untitled - VOCODER") 
+        self.file = None
+        self.txt_editor_field.delete(1.0,tk.END) 
+
+    def saveFile(self): 
+  
+        if self.file == None: 
+            # Save as new file 
+            self.file = asksaveasfilename(initialfile='Untitled.txt', 
+                                            defaultextension=".txt", 
+                                            filetypes=[("All Files","*.*"), 
+                                                ("Text Documents","*.txt")]) 
+  
+            if self.file == "": 
+                self.file = None
+
+            else: 
+                # Try to save the file 
+                file = open(self.file,"w") 
+                file.write(self.txt_editor_field.get(1.0,tk.END)) 
+                file.close() 
+                # Change the window title 
+                self.root.title(os.path.basename(self.file) + " - VOCODER") 
+                  
+        else: 
+            file = open(self.file,"w") 
+            file.write(self.thisTextArea.get(1.0,tk.END)) 
+            file.close() 
+  
+    def cut(self): 
+        self.txt_editor_field.event_generate("<<Cut>>") 
+  
+    def copy(self): 
+        self.txt_editor_field.event_generate("<<Copy>>") 
+  
+    def paste(self): 
+        self.txt_editor_field.event_generate("<<Paste>>") 
+
+    def setUpVoice(self):
+        tk.messagebox.showinfo("Test")
+
     def text_queue(self, thread_queue=None):
         result = vr.listen(self.txt_editor_field,self.cmd_receiver_txt,self.cmd_man_txt,self.sys_out_txt)
         thread_queue.put(result)
+
+    def image_resizer(self, e):
+        global img1, resized_img1, re_render
+        img1 = Image.open("assets/vocoder_icon_title.png")
+        resized_img1 = img1.resize((e.width, e.height), Image.ANTIALIAS)
+        re_render = ImageTk.PhotoImage(resized_img1)
+        self.img.configure(image=re_render)
 
     def update_text(self):
         self.loadred = Image.open("assets/redCircle.jpg")
@@ -132,7 +285,7 @@ class Application(tk.Frame):
             target=self.text_queue,
             kwargs={'thread_queue':self.thread_queue})
         self.new_thread.start()
-        self.after(100, self.listen_for_result)
+        self.root.after(1, self.listen_for_result)
 
     def listen_for_result(self):
         try:
@@ -144,24 +297,28 @@ class Application(tk.Frame):
                 self.txt_editor_field.insert(tk.END, text)
                 self.imggray.configure(image=self.rendergray)
         except queue.Empty:
-            self.after(100, self.listen_for_result)
+            self.root.after(1, self.listen_for_result)
 
     ## Handling end/compile buttons
     def change_indicator(self):
         vr.test_compiler(self.txt_editor_field.get(1.0,tk.END), self.terminal)
         self.imggray.configure(image=self.rendergray) 
-
-def on_closing():
+    
+    def on_closing(self):
         if messagebox.askokcancel("Quit", "Do you want to quit?"):
             os.popen('find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf')
-            root.destroy()
-          
-def main():
-    app.master.title("VOCODER")
-    root.protocol("WM_DELETE_WINDOW", on_closing)
-    app.mainloop()
+            self.root.destroy()
     
+def main():
+    dimension = []
+    for m in get_monitors():
+        if m.x == 0 and m.y == 0:
+            dimension.append(m.width)
+            dimension.append(m.height)
+    app = Application(width=dimension[0], height=dimension[1])
+    #app = Application(width=1600, height=900)
+    app.root.protocol("WM_DELETE_WINDOW", app.on_closing)
+    app.root.mainloop()
+
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = Application(master=root)
     main()
--- references/editor_module.py
+++ references/editor_module.py
@@ -5,19 +5,6 @@ from tkinter.messagebox import *
 from tkinter.filedialog import *
 import speech_recognition as sr
 
-def handling_sphinx():
-    # obtain audio from the microphone
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Say something!")
-        audio = r.listen(source)
-    # recognize speech using Sphinx
-    try:
-        print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-    except sr.UnknownValueError:
-        print("Sphinx could not understand audio")
-    except sr.RequestError as e:
-        print("Sphinx error; {0}".format(e))
 class Notepad: 
   
     __root = Tk() 
@@ -210,6 +197,5 @@ class Notepad:
   
 # Run main application 
 notepad = Notepad(width=600,height=400)
-handling_sphinx()
 notepad.run()
 
