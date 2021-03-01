a9282ec3701c624e627a6ae32d4ec464af719388
An <53161441+anbinhpham1107@users.noreply.github.com>
2021-02-28 21:37:22 -0600

    adding more dependency

--- dependency.bat
+++ dependency.bat
@@ -7,5 +7,8 @@ python -m pip install fuzzywuzzy
 python -m pip install google-api-python-client
 python -m pip install -U Sphinx
 python -m pip install screeninfo
+python -m pip install sounddevice
+python -m pip install scipy
+python -m pip install pydub
 echo Done!
 pause
